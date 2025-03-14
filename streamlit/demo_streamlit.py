af40e20c65227d49e6d745f24400cca3fadc9b48
WesleyG31 <wes.gb31@gmail.com>
2025-03-14 12:00:03 +0100

    streamlit

--- streamlit/demo_streamlit.py
+++ streamlit/demo_streamlit.py
@@ -18,7 +18,26 @@ modelos_disponibles = ["MobileNetV2 without weights", "Custom CNN"]
 # interface configuration
 st.title("🧥 Clothes clasification - Fashion MNIST 👕👖👟")
 st.write("Select a model and upload an image to classify the type of clothing.")
-st.write("### T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat','Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot")
+
+# classes
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Crear dos columnas en Streamlit
+col1, col2 = st.columns(2)
+
+# Dividir la lista en dos partes y mostrarlas en columnas separadas
+with col1:
+    for item in class_names[:5]:  # Primera mitad de la lista
+        st.write(f"- {item}")
+
+with col2:
+    for item in class_names[5:]:  # Segunda mitad de la lista
+        st.write(f"- {item}")
+
+
+
+
 st.write("The models were trained with the Fashion MNIST dataset.")
 st.write("#### MobileNetV2 without weights = 82.98% accuracy")   # Accuracy del modelo
 st.write("#### Custom CNN =  91.07% accuracy")   # Accuracy del modelo
@@ -29,9 +48,7 @@ modelo_seleccionado = st.selectbox("🔍 Chosse the model - Transfer Learning:",
 # load model based on selection
 modelo = cargar_modelo(modelo_seleccionado)
 
-# classes
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
-                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
 
 # upload image
 uploaded_file = st.file_uploader("📂 Upload a image...", type=["png", "jpg", "jpeg"])
